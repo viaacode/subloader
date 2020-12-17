87ca72db738c595798b87a84a1ca815749c4c839
Walter Schreppers <schreppers@gmail.com>
2020-12-17 15:23:35 +0100

    some more tests, more will follow

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,7 @@ import pytest
 
 from flask_api import status
 from app.subloader import app
-# from .fixtures import jwt_token
+from .fixtures import jwt_token
 
 
 @pytest.fixture(scope="module")
@@ -30,9 +30,47 @@ def test_liveness_check(client):
     assert res.status_code == status.HTTP_200_OK
 
 
-def test_authchecks(client):
+def test_search_media_security(client):
     res = client.get('/search_media')
     assert res.status_code == 401  # unauthorized
+
+
+def test_search_media(client):
+    res = client.get(f"/search_media?token={jwt_token()}")
+    assert res.status_code == 200
+
+
+def test_wrong_pid_entry(client):
+    res = client.post("/search_media", data={
+        'token': jwt_token(),
+        'department': 'testbeeld',
+        'pid': 'abc123'
+    }, follow_redirects=True)
+
+    assert res.status_code == 200
+    assert 'PID niet gevonden in testbeeld' in res.data.decode()
+
+
+def test_invalid_pid_entry(client):
+    res = client.post("/search_media", data={
+        'token': jwt_token(),
+        'department': 'testbeeld',
+        'pid': 'abc123#'
+    }, follow_redirects=True)
+
+    assert res.status_code == 200
+    assert 'PID formaat foutief' in res.data.decode()
+
+
+def test_working_pid_search(client):
+    res = client.post("/search_media", data={
+        'token': jwt_token(),
+        'department': 'testbeeld',
+        'pid': 'qsxs5jbm5c'
+    }, follow_redirects=True)
+
+    assert res.status_code == 200
+    assert 'Ondertitelbestand' in res.data.decode()
 
 
 def test_random_404(client, setup):
