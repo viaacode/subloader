a49152050a37a6a38935330e9c9947e0c6145add
Walter Schreppers <schreppers@gmail.com>
2021-11-12 15:49:49 +0100

    add reference to original saml demo code.

--- app/subloader.py
+++ app/subloader.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#  @Author: Walter Schreppers
+#  @Author: Walter Schreppers and integrated code from python-saml3 flask demo
 #
 #  app/subloader.py
 #
